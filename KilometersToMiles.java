856ce66718bafe186edc539fd5eb1a002d93a9ec
Rahul Gupta <156163350+Rahul20004@users.noreply.github.com>
2025-08-20 16:06:32 +0530

    Create KilometersToMiles.java

---
+++ KilometersToMiles.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class KilometersToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter distance in Kilometers: ");
+        double kilometers = sc.nextDouble();
+        double miles = kilometers * 0.621371;
+        System.out.println( miles + " Miles");
+
+        sc.close();
+    }
+}
