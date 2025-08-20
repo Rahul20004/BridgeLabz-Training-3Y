84af4309086afe95b4a59c550d9e50857c1f1969
Rahul Gupta <156163350+Rahul20004@users.noreply.github.com>
2025-08-20 17:21:11 +0530

    Update SamAverage.java

--- SamAverage.java
+++ SamAverage.java
@@ -3,6 +3,6 @@ public class SamAverage {
         int maths = 94, physics = 95, chemistry = 96;
         double average = (maths + physics + chemistry) / 3.0;
 
-        System.out.println(average);
+        System.out.println("Sam’s average mark in PCM is " + average);
     }
 }
